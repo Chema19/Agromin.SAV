62697a923baacf219007d125b039b9dd7f2ca6f2
josemaria inga villafuerte <chemalonso96@gmail.com>
2019-06-20 08:16:18 -0500

    changes

---
+++ Agromin.SAV.Api/Controllers/EntryExitProductApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Transactions;
+using System.Web.Http;
+
+namespace Agromin.SAV.Api.Controllers
+{
+    public class EntryExitProductApiController : BaseApiController
+    {
+        public IHttpActionResult ListEntryExitProduct() {
+            try {
+                using (var ts = new TransactionScope()) {
+
+                    response.Data = context.EntryExitProduct.Select(x => new { }).ToList();
+
+                    response.Error = false;
+                    response.Message = "Success";
+                    ts.Complete();
+                }
+                return Ok(response);
+            } catch (Exception ex) {
+                return Unauthorized();
+            }
+        }
+    }
+}
