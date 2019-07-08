f1c18930b88a7d25aed5a34c87c9c092b8a7721a
josemaria inga villafuerte <chemalonso96@gmail.com>
2019-07-08 02:04:34 -0500

    2:04

---
+++ Agromin.SAV.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agromin.SAV.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
